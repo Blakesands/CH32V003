1c2950c4aa5dec8e79f3f598f68b9d08f930f69c
Blakesands <71108094+Blakesands@users.noreply.github.com>
2023-03-21 00:43:18 +0000

    Update and rename main.c to MCP4131_Driver.c

--- MCP4131_Driver.c
+++ MCP4131_Driver.c
@@ -1,52 +1,18 @@
 /********************************** (C) COPYRIGHT *******************************
  * File Name          : main.c
- * Author             : WCH
+ * Author             : Green Dragom Maker Space
  * Version            : V1.0.0
- * Date               : 2022/08/08
- * Description        : Main program body.
-*********************************************************************************
-* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
-* Attention: This software (modified or not) and binary are used for 
-* microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ * Date               : 21/03/23
+ * Description        : Driver for MCP4131 Microchip Digital Potentiometer
 *******************************************************************************/
-
 /*
- *@Note
- single wire half duplex mode, master/slave mode, data transceiver:
- Master:SPI1_SCK(PC5)\SPI1_MOSI(PC6).
- Slave:SPI1_SCK(PC5)\SPI1_MISO(PC7).
-
- This routine demonstrates that Master sends and Slave receives.
- Note: The two boards download the Master and Slave programs respectively,
- and power on at the same time.
-     Hardware connection:PC5 -- PC5
-           PC7-- PC6
-
+Single wire half duplex mode
+CS (PC4) SCK(PC5) MOSI(PC6).
 */
 
 #include "debug.h"
 
-/* Global define */
-
-
-//#define increment 0
-//#define decrement 1
-
-/* Global Variable */
-volatile u8 Txval=0;
-
-/* SPI Communication Mode Selection */
-#define SPI_MODE   0
-
-u8 TxData[2] = { 0x04, 0x08};
-
-
-/*********************************************************************
- * @fn      SPI_1Lines_HalfDuplex_Init
- *
- * @brief   Configuring the SPI for half-duplex communication.
- *
- * @return  none */
+u8 TxData[2] = { 0x04, 0x08}; // 0x04 is the 8 bit increment command,  0x08 is the 8 bit decrement command
 
  void SPI_1Lines_HalfDuplex_Init(void)
 {
@@ -55,9 +21,6 @@ u8 TxData[2] = { 0x04, 0x08};
     NVIC_InitTypeDef NVIC_InitStructure={0};
 
     RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC | RCC_APB2Periph_SPI1, ENABLE );
-
-
-
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
@@ -90,17 +53,6 @@ u8 TxData[2] = { 0x04, 0x08};
     SPI_Cmd( SPI1, ENABLE );
 }
 
-
- /*     GPIO_Mode_AIN = 0x0,
-    GPIO_Mode_IN_FLOATING = 0x04,
-    GPIO_Mode_IPD = 0x28,
-    GPIO_Mode_IPU = 0x48,
-    GPIO_Mode_Out_OD = 0x14,
-    GPIO_Mode_Out_PP = 0x10,
-    GPIO_Mode_AF_OD = 0x1C,
-    GPIO_Mode_AF_PP = 0x18
- */
-
 void cs_init(){
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
@@ -117,7 +69,7 @@ void incrementZ(){
     GPIOC->BCR = 1 << 4; // put Chip Select pin C4 Low
     __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
     SPI_I2S_SendData( SPI1, TxData[0] );
-Delay_Us(9);
+    Delay_Us(9);
     GPIOC->BSHR = 1 << 4; // put Chip Select pin C4 High
     __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
 }
@@ -131,27 +83,15 @@ void decrementZ(){
     __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
 }
 
-
-/*********************************************************************
- * @fn      main **/
-
 int main(void)
 {
-//    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
     Delay_Init();
     cs_init();
     GPIOC->BSHR = 1 << 4; // put Chip Select pin C4 High
     SPI_1Lines_HalfDuplex_Init();
-      Delay_Ms(1000);
-//    GPIOC->BCR = 1 << 4; // put Chip Select pin C4 Low/
-//    __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
-//    Delay_Ms(1000);
+    Delay_Ms(1000);
 
-
-//    Delay_Ms(1000);
-//    SPI_I2S_ITConfig( SPI1, SPI_I2S_IT_TXE , ENABLE );
-
-    while(1)
+    while(1) // slowly changes resistance for testing
     {
         for (int i = 0; i < 130; ++i) {
             incrementZ();
@@ -163,6 +103,5 @@ int main(void)
             Delay_Ms(100);
         }
         Delay_Ms(1000);
-
     }
 }
