0ee0e8d125ccb32d62b45dc5f5e5027b2e3958b9
Blakesands <71108094+Blakesands@users.noreply.github.com>
2025-03-04 05:32:00 +0000

    Add files via upload

---
+++ CH224_PD_TRIGGER_CH32V003J4M6/PD_TRIGGER_WS2812_DRIVER.h
@@ -0,0 +1,69 @@
+/**
+ * If the LED doesn't work, check the timing of the data sent and adjust the NOPs in LED_SendBit.
+ * WS2812B LED expects 400n high/800n low for 0 and 800n high/400n low for a 1.
+ * Each bit should be 1200nS, Send 24 bits per led in the string with a 50uS gap to indicate end of frame
+ * Make sure you have defined SYSCLK_FREQ_48MHZ_HSI   48000000 // in system_ch32v00x.c
+ *
+**/
+
+// Send 1 bit to WS2812B LED.
+void LED_SendBit(uint8_t bit)
+
+{
+    if (bit)
+    {
+        GPIOD->BSHR = 1 << 6;           // put pin D6 high and wait for 800nS
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+            __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+        GPIOD->BCR = 1 << 6;            // put pin D6 low and wait for 400nS
+        return;                         // exit to LED_SendColour
+    }
+    GPIOD->BSHR = 1 << 6;               // put pin D6 high and wait for 400nS
+        __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+        __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+        __asm__("nop");__asm__("nop");
+    GPIOD->BCR = 1 << 6;                // put pin D6 low and wait for 800nS
+        __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+        __asm__("nop");__asm__("nop");__asm__("nop");__asm__("nop");
+}
+
+// Used for global brightness control
+int Map_Range(int value, int inMin, int inMax, int outMin, int outMax)
+{
+                                        // Make sure the input value is within the input range
+    value = value < inMin ? inMin : value;
+    value = value > inMax ? inMax : value;
+                                        // Map the value from the input range to the output range
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+// Send a single RGB colour for a single WS2812B LED
+void LED_SendColour(uint8_t Red, uint8_t Green, uint8_t Blue, uint8_t brightness)
+{
+    uint8_t green = Map_Range(Green, 0, 255, 0, brightness);
+    uint8_t red = Map_Range(Red, 0, 255, 0, brightness);
+    uint8_t blue = Map_Range(Blue, 0, 255, 0, brightness);
+
+                                        // Send the green component first
+    for (int i = 7; i >= 0; i--)
+    {
+        LED_SendBit((green >> i) & 1);  // Bitwise >> selects bit to send, & 1 tests if it's true
+    }
+                                        // Send the red component next
+    for (int i = 7; i >= 0; i--)
+    {
+        LED_SendBit((red >> i) & 1);
+    }
+                                        // Send the blue component last
+    for (int i = 7; i >= 0; i--)
+    {
+        LED_SendBit((blue >> i) & 1);
+    }
+}
---
+++ CH224_PD_TRIGGER_CH32V003J4M6/main.c
@@ -0,0 +1,61 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : Green Dragon
+ * Version            : V1.0.0
+ * Date               : 11/2024
+ * Description        : PD_Trigger PCB Driver
+ * https://radicalbakers.org/green-dragon-maker-space
+ ********************************************************************************/
+/**
+ * Parts used:  CH32V003 J4M6 RISC-V MCU 8 pin SO
+ *              CH224K USB PD Protocol Sink Controller 10 pin essop
+ *              NCP117-3.3 LDO Voltage Regulator
+ *              WS2812B Addressable LED
+ *              Button with 1k/1u RC debounce circuit
+ **/
+
+/* Includes */
+#include "debug.h"
+
+// Addressable LED Driver
+#include "PD_TRIGGER_WS2812_DRIVER.h"
+
+// PD_Trigger PCB driver
+#include "PD_TRIGGER_DRIVER.h"
+
+int main (void)
+{
+    Delay_Init();
+    LED_INIT();         // configure pin D6 as LED data output
+    POWERGOOD_INIT();   // configure pin D1 as an input from CH224 Power Good signal (active low)
+    PD_CONFIG_INIT();   // configure pins C1 C2 and C4 as PP outputs attached to cfg1 cfg2 cfg3 on the CH224
+    REQ_5V();           // set cfg pins to request 5V
+    EXTI0_INT_INIT();   // enable external hardware interrupt from button on A2
+
+    while(1)
+    {
+        __WFI();        // sleep mode waits for button press interrupt that increments V_REQ
+        LED_SendColour(0, 0, 0, 100); // resets LED
+        switch (V_REQ)  // requests voltages from source according to V_REQ
+        {
+            case 0:
+                REQ_5V();
+                break;
+            case 1:
+                REQ_9V();
+                break;
+            case 2:
+                REQ_12V();
+                break;
+            case 3:
+                REQ_15V();
+                break;
+            case 4:
+                REQ_20V();
+                break;
+            default:
+                REQ_5V();
+                break;
+        }           // end switch
+    }               // end while
+}                   // end main
